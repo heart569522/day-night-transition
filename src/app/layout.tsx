792ec89780d8b96dc35c2cf57590bb86bd2bd2f9
Heart Wirunrom <heart569522@gmail.com>
2023-06-17 03:54:16 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} theme`}>
+      <body className={`${inter.className} theme w-full h-screen`}>
         <ThemeBG />
         <Providers>
           <NavBar />
